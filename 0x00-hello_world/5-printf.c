c391e6551f59bad9c3a78a12916c2927985ed7ec
jackomosh <jackomosh6@gmail.com>
2023-10-08 13:19:37 -0700

    hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
