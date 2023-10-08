8ecfc182d2ee71e5b6ba860f6398d1a49d2e9485
jackomosh <jackomosh6@gmail.com>
2023-10-08 13:03:01 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 /**
- *
  * main - this is the main function
  *
  * Return: Always 0
- *
  */
 
 #include <stdio.h>
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
 }
