5bc9cd0e63e36d50303cec91d5c0acfff9d4e457
jackomosh <jackomosh6@gmail.com>
2023-10-19 01:40:28 -0700

    loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  * Return: Always 0
  */
 
-int main()
+int main(void)
 {
 _putchar('_');
 _putchar('p');
@@ -17,5 +17,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return(0);
+return (0);
 }
