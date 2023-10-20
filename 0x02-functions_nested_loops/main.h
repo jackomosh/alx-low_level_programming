db7d26710d26eab8ab3f1b3c850cddae7f880b62
jackomosh <jackomosh6@gmail.com>
2023-10-20 06:43:52 -0700

    loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,7 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
-print_alphabet();
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -14,5 +13,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
 #endif
