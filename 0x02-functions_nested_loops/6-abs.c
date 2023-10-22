8a11c4c13e832e1a7d789a25a30bccabce5fe7a3
jackomosh <jackomosh6@gmail.com>
2023-10-22 01:51:42 -0700

    loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _abs - this is the function of the task
+ *  @a: - this is our function  parameter
+ *  Return: -a or a;
+ */
+int _abs(int a)
+{
+if (a < 0)
+return (-a);
+else if (a >= 0)
+{
+return (a);
+}
+return (0);
+}
