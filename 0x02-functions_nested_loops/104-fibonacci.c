4c5bc771f367c31a31c90c593d0bae6bf13c0d24
jackomosh <jackomosh6@gmail.com>
2023-10-22 10:21:56 -0700

    loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -22,13 +22,13 @@ int main(void)
 	}
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
 	for (count = 93; count < 99; count++)
 	{
 	half1 = fib1_half1 + fib2_half1;
-	half2 = fib1_half2 + fib2_half2;
-	if (fib1_half2 + fib2_half2 > 9999999999)
+	half2 = fib2_half2 + fib2_half2;
+	if (fib2_half2 + fib2_half2 > 9999999999)
 	{
 	half1 += 1;
 	half2 %= 10000000000;
@@ -37,7 +37,7 @@ int main(void)
 	if (count != 98)
 	printf(", ");
 	fib1_half1 = fib2_half1;
-	fib1_half2 = fib2_half2;
+	fib2_half2 = fib2_half2;
 	fib2_half1 = half1;
 	fib2_half2 = half2;
 	}
