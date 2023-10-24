6a2778aec9e790a729b7242382a39f4233e73011
jackomosh <jackomosh6@gmail.com>
2023-10-24 13:54:00 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - main function
+ * @c: - function parameter
+ * Return: 1 if uppercase
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{	
+	return (1);
+}
+else
+	return (0);
+}
